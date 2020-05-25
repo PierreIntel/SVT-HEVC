7c8293cbd9a192c608e821a9a8cee022f4eaf863
Pierre Vallée <pierre.vallee@intel.com>
2020-05-25 11:33:02 +0200

    correction

--- Source/Lib/ASM_SSSE3/EbAvcStyleMcp_Intrinsic_SSSE3.c
+++ Source/Lib/ASM_SSSE3/EbAvcStyleMcp_Intrinsic_SSSE3.c
@@ -137,10 +137,10 @@ void AvcStyleLumaInterpolationFilterVertical_SSSE3_INTRIN(
                 ref2s_512 = _mm512_loadu_si512((__m512i *)(refPicTemp + 2 * srcStride));
                 ref3s_512 = _mm512_loadu_si512((__m512i *)(refPicTemp + 3 * srcStride));
 
-                ref0_1_lo_512 = _mm512_unpacklo_epi8(ref0, refs);
-                ref2_3_lo_512 = _mm512_unpacklo_epi8(ref2s, ref3s);
-                ref0_1_hi_512 = _mm512_unpackhi_epi8(ref0, refs);
-                ref2_3_hi_512 = _mm512_unpackhi_epi8(ref2s, ref3s);
+                ref0_1_lo = _mm512_unpacklo_epi8(ref0, refs);
+                ref2_3_lo = _mm512_unpacklo_epi8(ref2s, ref3s);
+                ref0_1_hi = _mm512_unpackhi_epi8(ref0, refs);
+                ref2_3_hi = _mm512_unpackhi_epi8(ref2s, ref3s);
 
                 sum_lo = _mm512_add_epi16(_mm512_maddubs_epi16(ref0_1_lo,IFCoeff_1_0_512),
                                         _mm512_maddubs_epi16(ref2_3_lo, IFCoeff_3_2_512));
